4b2b05b17e23d3e11d71f14af693259e80ea636f
salahqudah <salahqudah13@gmail.com>
2025-06-03 14:02:19 +0300

    Function-Based Views

--- my_tennis_club/members/urls.py
+++ my_tennis_club/members/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from . import views
-
+from .views import hello_world
 urlpatterns = [
     path('', views.main, name='main'),
     path('members/', views.members, name='members'),  # Remove the 'members/' prefix here
     path('members/details/<int:id>', views.details, name='details'),
     path('testing/', views.testing, name='testing'),
     path('orderbyfirst/', views.orderbyfirst, name='orderbyfirst'),  
-    path('descendingby/',views.descendingby,name='descendingby')
+    path('descendingby/',views.descendingby,name='descendingby'),
+    path('', views.homepage, name='home'),
+    path('api/hello/',hello_world,name='hello_world ')
+   
 ]
--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -2,7 +2,8 @@ from django.http import HttpResponse,HttpResponseRedirect
 from django.template import loader
 from .models import Member
 from datetime import datetime,date
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 def members(request):
   mymembers = Member.objects.all().values()
   template = loader.get_template('all_members.html')
@@ -44,6 +45,20 @@ def descendingby(request):
     'mymembers':mydata,
   }
   return HttpResponse(template.render(context,request))
+
+
+def homepage(request):
+    template = loader.get_template('home.html')
+    return HttpResponse(template.render({}, request))
+
+
+@api_view(['GET', 'POST'])
+def hello_world(request):
+    if request.method == 'GET':
+        return Response({"message": "Hello, world!"})
+    elif request.method == 'POST':
+        # echo back the posted data (JSON is parsed into request.data)
+        return Response({"message": "You posted:", "data": request.data})
 
   
 
